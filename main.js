93b869b41b02f8565aeda3429b5cf704f39327f4
sarahfalter <sarah.falter@ufl.edu>
2021-07-19 16:17:48 -0400

    Add markup, javascript, and styling

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+const btn = document.getElementById('clickme');
+
+document.getElementsByTagName('main').innerHTML = 
+btn.addEventListener('click', function() {
+    let itemsRandom = ['stupify!', 'Aguamenti', 'Alohomora', 'Episky', 'Accio Butterbeer!', 'Immobulus!', 'Confundo!', 'Descendo!', 'Finite Incantatem!', 'Lumos Maxima!'];
+    itemsRandom.sort((a, b) => a - b);
+
+});
+
+
+
+
+
+
+
---
+++ src/main.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("clickme");
+
+btn.addEventListener('click', function() {
+    let itemsRandom = ['stupify!', 'Aguamenti', 'Alohomora', 'Episky', 'Accio Butterbeer!', 'Immobulus!', 'Confundo!', 'Descendo!', 'Finite Incantatem!', 'Lumos Maxima!'];
+    itemsRandom.sort(function(a, b){return 0.5 - Math.random()});
+    
+    for (var i = 0; i < itemsRandom.length; i++) {
+        document.getElementById("content").innerHTML = itemsRandom[i];
+      }
+
+});
+
